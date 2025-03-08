ddb0bfc6f7e434c487aa1d1bd0290c0715c63afa
Aguren <sentientfishsentient@gmail.com>
2025-03-08 01:09:57 -0800

    feat: doll working parsing

--- src/doll.py
+++ src/doll.py
@@ -18,27 +18,64 @@ This includes:
 
 import wikitextparser as wtp
 
+class Node:
+    """
+    Class representation of a doll's neural node
+    """
+
+    def __init__(self, name, desc, position):
+        self.name = name
+        self.desc = desc
+        self.position = position
+    
+
+    def __str__(self):
+        return f"name: {self.name}\ndesc: {self.desc}\nposition: {self.position}\n"
+
+
+class Skill:
+    """
+    Internal representation of a doll's skill
+    """
+
+    def __init__(self, name, desc, extra_effects):
+        self.name = name
+        self.desc = desc
+        self.extra_effects = extra_effects
+    
+    
+    def __str__(self):
+        return f"name: {self.name}\ndesc: {self.desc}\nextra_effects: {self.extra_effects}\n"
+
+
 class Doll:
     """
     Doll class definition
     """
 
-    class Node:
+    # Internal variables to parse wikitext
+    _VALUE_INDEX = 1
+    _WEAK_ICON_STRING = "GFL2WeakIcon"
+    _IGNORED_KEYS = [
+        "icon",
+        "skilllevelcount",
+    ]
+    _KEY_INDEX = 0
+    _FIRST_VALUE_INDEX = 1
+    _SIMPLE_DATA_ARRAY_LENGTH = 2
+
+    class SKILL_PARAMETER_STRING:
         """
-        Internal node class representation
+        Internal class to denote skill table fields
         """
 
-        def __init__(self, name, desc, position):
-            self.name = name
-            self.desc = desc
-            self.position = position
-        
-        def __str__(self):
-            return f"name: {self.name}, desc: {self.desc}, position: {self.position}"
-        
+        NAME_STRING = "name"
+        TEXT_STRING = "text"
+        EXTRA_EFFECT_STRING = "extraeffect"
 
-    def __init__(self, doll_wikitext):
-        template = self._get_base_template(doll_wikitext)
+
+    def __init__(self, doll_data, skill_data_array):
+        template = self._get_base_template(doll_data)
 
         self.full_name = self._get_template_param_value(template, "fullname")
         self.role = self._get_template_param_value(template, "role")
@@ -51,6 +88,7 @@ class Doll:
         self.gfl_name = self._get_template_param_value(template, "GFL")
 
         self.nodes = self._get_nodes(template)
+        self.skills = self._get_skills(skill_data_array)
 
 
     def _get_node(self, template, node_position, is_key=False, key_position=0):
@@ -73,7 +111,7 @@ class Doll:
             desc_string += f"{key_position}"
         
         desc = self._get_template_param_value(template, desc_string)
-        node = self.Node(name, desc, node_position)
+        node = Node(name, desc, node_position)
 
         return node
 
@@ -101,6 +139,141 @@ class Doll:
         ]
 
         return nodes
+
+
+    def _get_skills(self, skill_data_array):
+        skills = []
+        for skill_data in skill_data_array:
+            
+            skill = self._parse_skill_table(skill_data)
+            skills.append(skill)
+        
+        return skills
+    
+
+    def _table_data_to_dict(self, table_data):
+        """
+        list of table data to dictionary
+        """
+
+        table_dictionary = {}
+        for data in table_data:
+            key = data[Doll._KEY_INDEX]
+            data_array_length = len(data)
+            
+            value = ""
+            if (data_array_length > Doll._SIMPLE_DATA_ARRAY_LENGTH):
+                value = []
+
+                for i in range(Doll._FIRST_VALUE_INDEX, data_array_length):
+                    data_val = data[i]
+                    value.append(data_val)
+            else:
+                value = data[Doll._FIRST_VALUE_INDEX]
+
+            table_dictionary[key] = value
+        
+        return table_dictionary
+    
+
+    def _simplify(self, wikitext):
+        wikitext = self._remove_wikilinks(wikitext)
+        wikitext = self._remove_templates(wikitext)
+
+        return wikitext
+
+
+    def _remove_wikilinks(self, wikitext):
+        wikilinks = wtp.parse(wikitext).wikilinks
+
+        for wikilink in wikilinks:
+            wikilink_str = str(wikilink)
+            wikitext = wikitext.replace(wikilink_str, wikilink.text)
+        
+        return wikitext
+
+
+    def _remove_templates(self, wikitext):
+        templates = wtp.parse(wikitext).templates
+
+        for template in templates:
+            template_str = str(template)
+
+            template_value = ""
+            if Doll._WEAK_ICON_STRING not in template:        
+                template_value = template.arguments[Doll._VALUE_INDEX].value
+
+            wikitext = wikitext.replace(template_str, template_value)
+        
+        return wikitext
+
+
+    def _parse_skill_table(self, skill_data):
+        table_data = wtp.Table(skill_data).data(span=False)
+        skill_table_dictionary = self._table_data_to_dict(table_data)
+
+        parsed_skill_dictionary = {}
+        variable_skill_parameters = []
+        for key in skill_table_dictionary:
+            if key in Doll._IGNORED_KEYS:
+                continue
+            
+            value = skill_table_dictionary[key]
+            
+            match key:
+                case self.SKILL_PARAMETER_STRING.NAME_STRING:
+                    parsed_skill_dictionary[key] = value
+                case self.SKILL_PARAMETER_STRING.TEXT_STRING:
+                    skill_text = self._simplify(value)
+
+                    parsed_skill_dictionary[key] = skill_text
+                case self.SKILL_PARAMETER_STRING.EXTRA_EFFECT_STRING:
+                    extra_effects = skill_table_dictionary[key]
+
+                    extra_effects_array = []
+                    for extra_effect in extra_effects:
+                        if extra_effect == "":
+                            continue
+
+                        cleaned_extra_effect = self._simplify(extra_effect)
+                        extra_effects_array.append(cleaned_extra_effect)
+                    
+                    parsed_skill_dictionary[key] = extra_effects_array
+                case _:
+                    # We found some variables, save them so we can replace it later
+                    variable_skill_parameters.append(key)
+        
+        # Remove the extraeffects variable from skill text
+        skill_text = parsed_skill_dictionary[self.SKILL_PARAMETER_STRING.TEXT_STRING]
+
+        extraeffects_string = f"(${self.SKILL_PARAMETER_STRING.EXTRA_EFFECT_STRING})"
+        skill_text = skill_text.replace(extraeffects_string, "")
+
+        # Replace the variable skill parameter
+        for parameter in variable_skill_parameters:
+            parameter_string = f"(${parameter})"
+            
+            parameter_array = skill_table_dictionary[parameter]
+            parameter_value = ""
+
+            for value in parameter_array:
+                value = self._simplify(value)
+                parameter_value += f"{value}/"
+            
+            parameter_value = parameter_value[:-1]
+
+            skill_text = skill_text.replace(parameter_string, parameter_value)
+        
+        parsed_skill_dictionary[self.SKILL_PARAMETER_STRING.TEXT_STRING] = skill_text
+        
+        skill = Skill(
+            name=parsed_skill_dictionary[self.SKILL_PARAMETER_STRING.NAME_STRING],
+            desc=parsed_skill_dictionary[self.SKILL_PARAMETER_STRING.TEXT_STRING],
+            extra_effects=parsed_skill_dictionary.get(self.SKILL_PARAMETER_STRING.\
+                    EXTRA_EFFECT_STRING, None),
+        )
+
+        return skill
 
 
     def _get_base_template(self, wikitext):
